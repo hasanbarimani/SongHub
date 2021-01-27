27893a4e9131039fc64c709a04c86c97a70a41d8
barimanihasan <barimanihasan1378@gmail.com>
2021-01-27 15:38:47 +0330

    PopulateGigForm

---
+++ gighub/Controllers/GigsController.cs
@@ -0,0 +1,30 @@
+﻿using gighub.Models;
+using gighub.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gighub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()    
+        {
+            GigFormViewModel viewModel = new GigFormViewModel
+
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
