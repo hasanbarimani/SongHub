497c295b28176d1f3065785230e614d70da5870f
barimanihasan <barimanihasan1378@gmail.com>
2021-02-02 20:08:56 +0330

    some style change

--- gighub/Controllers/HomeController.cs
+++ gighub/Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace gighub.Controllers
         {
             var upcomingGig = _context.Gigs
                 .Include(g => g.Artist)
+                .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
             return View(upcomingGig);
         }
